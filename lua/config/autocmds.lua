c5109a6118acb1d06e17690c6661462a3f1023db
eet cake <keyboardshortcake@gmail.com>
2023-04-03 03:53:24 +0000

    removed transparent background stuff

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -57,7 +57,3 @@ vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
     end
   end,
 })
-
--- vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
--- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
-
